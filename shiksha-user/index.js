46829aa7d09b376dda73bcca502bd4d7c70928be
Shubham <shubham.jha@tekditechnologies.com>
2025-01-03 15:04:26 +0530

    User Service Package

---
+++ shiksha-user/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const router = express.Router()
+const routes = require('./constants/routes')
+const packageRouter = require('./router')
+
+// const getDependencies = () => {
+// 	return ['kafka', 'kafka-connect', 'redis']
+// }
+
+const getPackageMeta = () => {
+	return {
+		basePackageName: 'user',
+		packageName: 'shiksha-user',
+	}
+}
+
+const createPackage = (options) => {
+	return {
+		router: () => {
+			console.log('router')
+		},
+		endpoints: [],
+		dependencies: [],
+	}
+}
+
+router.get('/', (req, res) => {
+	res.send('Hello, world! From package')
+})
+
+module.exports = {
+	dependencies: getDependencies(),
+	routes,
+	createPackage,
+	packageMeta: getPackageMeta(),
+	packageRouter,
+}
